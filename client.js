d537b5af5058467624aff2299fbb36a7dcdf9172
jeremy <nashoba.jeremy@gmail.com>
2024-11-12 16:45:27 +0100

    Initial commit

---
+++ client.js
@@ -0,0 +1,15 @@
+const socket = io("wss://server-chat.jeremyduc.com");
+const button = document.querySelector("button");
+socket.on('message', (message) => {
+    console.log(message);
+    const ligne = document.createElement('li')
+    ligne.innerHTML = message.author+ " a écrit : "+message.content;
+    document.querySelector('ul').appendChild(ligne)
+})
+
+button.addEventListener('click', (e) => {
+    const toSend = document.querySelector('input').value;
+    socket.emit('message', {
+        content:toSend,
+    });
+})
